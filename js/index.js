585ea71b4219628f1b65b1aedb86c9742cd8f416
lding5 <lindading5@gmail.com>
2014-11-26 11:49:09 -0500

    hack1 html updates

--- js/index.js
+++ js/index.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
   console.log("index.js load");
   $("#navigationBar").load("navbar.html");
 
+  var URL = document.URL;
+  if (URL.indexOf("index.html") > -1){
+  	$("#navIndex").addClass("active");
+  	console.log("navbar index");
+  } else if (URL.indexOf("about.html") > -1){
+  	$("#navAbout").addClass("active");
+  	console.log("navbar about");
+  }
+
 });
